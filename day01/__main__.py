72ab14531e7ad6181ba6b4a0e6d2edf7ce182822
Scott Odle <scott@sjodle.com>
2024-11-30 22:13:52 -0700

    day 1

--- day01/__main__.py
+++ day01/__main__.py
@@ -1,2 +1,33 @@
+import sys
+
+
+def read_lists() -> tuple[list[int], list[int]]:
+    left = []
+    right = []
+    for line in sys.stdin.readlines():
+        nl, nr = line.split()
+        left.append(int(nl))
+        right.append(int(nr))
+    return left, right
+
+
+def part1(left: list[int], right: list[int]) -> int:
+    pairs = zip(sorted(left), sorted(right))
+    result = 0
+    for l, r in pairs:
+        result += abs(l-r)
+    return result
+
+
+def part2(left: list[int], right: list[int]) -> int:
+    result = 0
+    for l in left:
+        r = len([n for n in right if n==l])
+        result += l*r
+    return result
+
+
 if __name__ == "__main__":
-    print("yeet")
+    left, right = read_lists()
+    print(f"Part 1:\t{part1(left, right)}")
+    print(f"Part 2:\t{part2(left, right)}")
