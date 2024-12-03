7a1efcf27b1eb71f89c7b8b05f5406c46f1e7e08
Scott Odle <scott@sjodle.com>
2024-12-02 22:35:04 -0700

    day 3 part 2

--- day03/__main__.py
+++ day03/__main__.py
@@ -2,6 +2,7 @@ import sys
 import re
 
 re_mul = re.compile(r"mul\((\d+),(\d+)\)")
+re_instruction = re.compile(r"(mul\(\d+,\d+\)|do\(\)|don't\(\))")
 
 
 def read_mults() -> list[tuple[int, int]]:
@@ -14,13 +15,41 @@ def read_mults() -> list[tuple[int, int]]:
     return out
 
 
-def part1(mults: list[tuple[int, int]]) -> int:
+def read_instructions() -> list[str]:
+    out = []
+    for line in sys.stdin.readlines():
+        out += [match.group(0) for match in re_instruction.finditer(line)]
+    return out
+
+
+def part1(instructions: list[str]) -> int:
     out = 0
-    for lhs, rhs in mults:
-        out += lhs * rhs
+
+    for instruction in instructions:
+        if instruction.startswith("mul"):
+            lhs, rhs = re_mul.match(instruction).groups()
+            out += int(lhs) * int(rhs)
+
+    return out
+
+
+def part2(instructions: list[str]) -> int:
+    enabled = True
+    out = 0
+
+    for instruction in instructions:
+        if instruction == "do()":
+            enabled = True
+        elif instruction == "don't()":
+            enabled = False
+        elif enabled:
+            lhs, rhs = re_mul.match(instruction).groups()
+            out += int(lhs) * int(rhs)
+
     return out
 
 
 if __name__ == "__main__":
-    mults = read_mults()
-    print(f"Part 1:\t{part1()}")
+    instructions = read_instructions()
+    print(f"Part 1:\t{part1(instructions)}")
+    print(f"Part 2:\t{part2(instructions)}")
