b9a5252714a228359aa67b2ee9ada51b125e499d
Scott Odle <scott@sjodle.com>
2024-12-08 18:13:06 -0700

    day8 part2

--- day08/__main__.py
+++ day08/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 Coordinate = tuple[int, int]
 AntennaMap = dict[str, set[Coordinate]]
@@ -26,7 +27,7 @@ def read_antennae() -> tuple[AntennaMap, int, int]:
 def part1(antennae: AntennaMap, height: int, width: int) -> int:
     antinodes = set()
 
-    for freq, coordinates in antennae.items():
+    for _, coordinates in antennae.items():
         for y1, x1 in coordinates:
             for y2, x2 in coordinates:
                 rise = y2 - y1
@@ -46,6 +47,36 @@ def part1(antennae: AntennaMap, height: int, width: int) -> int:
     return len(antinodes)
 
 
+def part2(antennae: AntennaMap, height: int, width: int) -> int:
+    antinodes = set()
+
+    for _, coordinates in antennae.items():
+        for a, b in itertools.product(coordinates, coordinates):
+            if a == b:
+                continue
+
+            y0, x0 = a
+            y1, x1 = b
+
+            rise = y1 - y0
+            run = x1 - x0
+
+            y, x = y0, x0
+            while y in range(height) and x in range(width):
+                antinodes.add((y, x))
+                y -= rise
+                x -= run
+
+            y, x = y0, x0
+            while y in range(height) and x in range(width):
+                antinodes.add((y, x))
+                y += rise
+                x += run
+
+    return len(antinodes)
+
+
 if __name__ == "__main__":
     antennae, height, width = read_antennae()
     print(f"Part 1:\t{part1(antennae, height, width)}")
+    print(f"Part 2:\t{part2(antennae, height, width)}")
