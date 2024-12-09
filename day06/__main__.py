92f9ae3b344b423b671245b9b5e23ffb75d925f1
Scott Odle <scott@sjodle.com>
2024-12-09 16:06:43 -0700

    day6 part2 slow

--- day06/__main__.py
+++ day06/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import itertools
 
 
 Coordinate = tuple[int, int]
@@ -48,6 +49,41 @@ def part1(
     return len(visited)
 
 
+def part2(
+    guard_position: Coordinate, obstacles: list[Coordinate], dimensions: Coordinate
+) -> int:
+    height, width = dimensions
+
+    viable_obstacles = set()
+    for new_obstacle in itertools.product(range(height), range(width)):
+        if new_obstacle in obstacles or new_obstacle == guard_position:
+            continue
+
+        y, x = guard_position
+        orientation = 90  # guard starts facing up
+
+        visited = set()
+        while x in range(width) and y in range(height):
+            norm_orientation = orientation % 360
+            if (y, x, norm_orientation) in visited:
+                viable_obstacles.add(new_obstacle)
+                break
+
+            visited.add((y, x, norm_orientation))
+
+            look_x = x + int(math.cos(math.radians(orientation)))
+            look_y = y - int(math.sin(math.radians(orientation)))
+
+            if (look_y, look_x) in [*obstacles, new_obstacle]:
+                orientation -= 90
+            else:
+                x = look_x
+                y = look_y
+
+    return len(viable_obstacles)
+
+
 if __name__ == "__main__":
     guard_position, obstacles, dimensions = read_maze()
     print(f"Part 1:\t{part1(guard_position, obstacles, dimensions)}")
+    print(f"Part 2:\t{part2(guard_position, obstacles, dimensions)}")
