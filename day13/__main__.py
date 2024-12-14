e230de9a8149f0d6c6c8af27d0387957ca31a8bc
Scott Odle <scott@sjodle.com>
2024-12-14 14:18:54 -0700

    day13 part 2 and faster part1

--- day13/__main__.py
+++ day13/__main__.py
@@ -4,6 +4,7 @@ import re
 from typing import NamedTuple, Optional
 
 import networkx as nx
+from ortools.sat.python import cp_model
 
 button_regex = re.compile(r"Button [AB]: X\+(\d+), Y\+(\d+)")
 goal_regex = re.compile(r"Prize: X=(\d+), Y=(\d+)")
@@ -46,6 +47,39 @@ class CraneMachine(NamedTuple):
 
         return G
 
+    def solve_constraint(self, part2: bool = False) -> int:
+        goal_x, goal_y = self.goal
+
+        if part2:
+            goal_x += 10000000000000
+            goal_y += 10000000000000
+
+        dx_a, dy_a = self.button_a
+        dx_b, dy_b = self.button_b
+
+        model = cp_model.CpModel()
+
+        a_presses = model.new_int_var(0, 10000000000000, "a_presses")
+        b_presses = model.new_int_var(0, 10000000000000, "b_presses")
+        x = model.new_int_var(0, cp_model.INT_MAX // 4, "x")
+        y = model.new_int_var(0, cp_model.INT_MAX // 4, "y")
+        tokens = model.new_int_var(0, cp_model.INT_MAX // 4, "tokens")
+
+        model.add(x == goal_x)
+        model.add(y == goal_y)
+        model.add(x == dx_a * a_presses + dx_b * b_presses)
+        model.add(y == dy_a * a_presses + dy_b * b_presses)
+        model.add(tokens == 3 * a_presses + b_presses)
+
+        model.minimize(tokens)
+
+        solver = cp_model.CpSolver()
+        status = solver.solve(model)
+
+        if status == cp_model.OPTIMAL:
+            return solver.value(tokens)
+        return 0
+
 
 def read_input() -> list[CraneMachine]:
     lines = sys.stdin.readlines()
@@ -78,15 +112,19 @@ def part1(machines: list[CraneMachine]) -> int:
     acc = 0
 
     for machine in machines:
-        G = machine.generate_movesets()
-        try:
-            acc += nx.shortest_path_length(G, (0, 0), machine.goal, weight="cost")
-        except nx.NetworkXNoPath:
-            pass
+        acc += machine.solve_constraint()
 
+    return acc
+
+
+def part2(machines: list[CraneMachine]) -> int:
+    acc = 0
+    for machine in machines:
+        acc += machine.solve_constraint(True)
     return acc
 
 
 if __name__ == "__main__":
     machines = read_input()
     print(f"Part 1:\t{part1(machines)}")
+    print(f"Part 2:\t{part2(machines)}")
