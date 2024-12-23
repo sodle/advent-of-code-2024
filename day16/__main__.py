4596de246124ae12ea04172de554b7f02058933d
Scott Odle <scott@sjodle.com>
2024-12-23 12:20:34 -0700

    day16 ** slow

--- day16/__main__.py
+++ day16/__main__.py
@@ -1,7 +1,7 @@
 import sys
 from math import inf
 
-from networkx import DiGraph, shortest_path_length, NetworkXNoPath
+from networkx import DiGraph, shortest_path_length, all_shortest_paths, NetworkXNoPath
 
 from typing import NamedTuple
 
@@ -40,6 +40,23 @@ def read_input() -> Maze:
 
 
 def part1(maze: Maze) -> int:
+    start, end, G = build_graph(maze)
+
+    start_oriented = (*start, "E")
+    shortest = inf
+    for end_direction in "NESW":
+        end_oriented = (*end, end_direction)
+        try:
+            shortest_dir = shortest_path_length(G, start_oriented, end_oriented, "cost")
+            if shortest_dir < shortest:
+                shortest = shortest_dir
+        except NetworkXNoPath:
+            pass
+
+    return shortest
+
+
+def build_graph(maze) -> tuple[Coordinate, Coordinate, DiGraph]:
     start, end, walls = maze
 
     G = DiGraph()
@@ -68,20 +85,31 @@ def part1(maze: Maze) -> int:
                 G.add_edge((x, y, "S"), (x, y + 1, "S"), cost=1)
             if y - 1 in height and (x, y - 1) not in walls:
                 G.add_edge((x, y, "N"), (x, y - 1, "N"), cost=1)
+    return start, end, G
+
+
+def part2(maze) -> int:
+    start, end, G = build_graph(maze)
 
     start_oriented = (*start, "E")
-    shortest = inf
+    paths_by_length: dict[int, set[Coordinate]] = {}
     for end_direction in "NESW":
         end_oriented = (*end, end_direction)
         try:
-            shortest_dir = shortest_path_length(G, start_oriented, end_oriented, "cost")
-            if shortest_dir < shortest:
-                shortest = shortest_dir
+            length = shortest_path_length(G, start_oriented, end_oriented, "cost")
+            if length not in paths_by_length:
+                paths_by_length[length] = set()
+            for path in all_shortest_paths(G, start_oriented, end_oriented, "cost"):
+                for x, y, _ in path:
+                    paths_by_length[length].add((x, y))
         except NetworkXNoPath:
             pass
-    return shortest
+
+    shortest = min(*paths_by_length.keys())
+    return len(paths_by_length[shortest])
 
 
 if __name__ == "__main__":
     maze = read_input()
     print(f"Part 1:\t{part1(maze)}")
+    print(f"Part 2:\t{part2(maze)}")
